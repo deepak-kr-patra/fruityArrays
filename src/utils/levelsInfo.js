087a6149e8e3fa1f025b3885bd5858f112d3ad55
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2025-08-08 22:33:24 +0530

    added two more levels and optimized CodeEditor component

--- src/utils/levelsInfo.js
+++ src/utils/levelsInfo.js
@@ -11,8 +11,20 @@ const levelsInfo = [
         expectedFruits: ['🍎', '🍌', '🍇', '🍓', '🍍'],
         levelDescription: 'Use pop method to remove "🥭" from the fruits array.'
     },
-]
+    // level 3 info at index 2
+    {
+        defaultFruits: ['🍇', '🍊', '🍎', '🍓', '🍍'],
+        expectedFruits: ['🍉', '🍇', '🍊', '🍎', '🍓', '🍍'],
+        levelDescription: 'Use unshift method to add "🍉" to the left of fruits array.'
+    },
+    // level 4 info at index 3
+    {
+        defaultFruits: ['🍎', '🍌', '🍇', '🍓', '🍍', '🥭'],
+        expectedFruits: ['🍌', '🍇', '🍓', '🍍', '🥭'],
+        levelDescription: 'Use shift method to remove "🍎" from the left of fruits array.'
+    },
+];
 
 export const getLevelInfo = (level) => {
     return levelsInfo[level - 1];
-}
+};
