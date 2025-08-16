22ab4b513b5548118177dc306c63ab6be44fd917
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2025-08-16 23:50:56 +0530

    removed auto-closing feature from CodeEditor textarea

---
+++ src/components/LevelsModal.jsx
@@ -0,0 +1,10 @@
+const LevelsModal = () => {
+    return (
+        <div className="absolute top-[38px] flex flex-col items-center">
+            <div className="absolute w-8 h-8 bg-amber-500 rotate-45"></div>
+            <div className="absolute top-1.5 h-[200px] bg-blue-500">LevelsModal</div>
+        </div>
+    )
+}
+
+export default LevelsModal
